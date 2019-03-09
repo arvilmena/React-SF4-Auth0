dbd0392d36fe8b1318ea96a0b2dc1c4e342b0f9b
Arvil Mena <arvilmena@gmail.com>
2019-03-09 18:41:36 +0000

    composer require symfony/maker-bundle --dev

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Auth0\JWTAuthBundle\JWTAuthBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
