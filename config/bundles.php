abd7e1123696966657d206148ee3a0014e9d1bd8
Arvil Mena <arvilmena@gmail.com>
2019-03-09 18:48:16 +0000

    composer require symfony/security-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Auth0\JWTAuthBundle\JWTAuthBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
