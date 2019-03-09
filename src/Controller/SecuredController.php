f1b3d738f7dad429c61a1b6e95a594bad4a40ced
Arvil Mena <arvilmena@gmail.com>
2019-03-09 18:46:51 +0000

    /api/public + /api/private @SecuredController.php

--- src/Controller/SecuredController.php
+++ src/Controller/SecuredController.php
@@ -8,13 +8,58 @@ use Symfony\Component\Routing\Annotation\Route;
 class SecuredController extends AbstractController
 {
     /**
-     * @Route("/secured", name="secured")
+     * @Route("/api/public", name="api/public")
      */
-    public function index()
+    public function publicAction()
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SecuredController.php',
+            [
+                'albumId' => "1",
+                "id" => 1,
+                "title" => "accusamus beatae ad facilis cum similique qui sunt",
+                "description" => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout"
+            ],
+            [
+                'albumId' => "2",
+                "id" => 2,
+                "title" => "accusamus beatae ad facilis cum similique qui sunt",
+                "description" => "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text"
+            ],
+            [
+                'albumId' => "3",
+                "id" => 3,
+                "title" => "accusamus beatae ad facilis cum similique qui sunt",
+                "description" => "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form"
+            ],
+        ]);
+    }
+
+    /**
+     * @Route("/api/private", name="api/private")
+     *
+     * @return JSONResponse
+     */
+    public function privateAction()
+    {
+        return $this->json([
+            [
+                'albumId' => "1",
+                "id" => 1,
+                "title" => "accusamus beatae ad facilis cum similique qui sunt",
+                "description" => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout"
+            ],
+            [
+                'albumId' => "2",
+                "id" => 2,
+                "title" => "accusamus beatae ad facilis cum similique qui sunt",
+                "description" => "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text"
+            ],
+            [
+                'albumId' => "3",
+                "id" => 3,
+                "title" => "accusamus beatae ad facilis cum similique qui sunt",
+                "description" => "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form"
+            ],
         ]);
     }
 }
